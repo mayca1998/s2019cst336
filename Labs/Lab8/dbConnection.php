b3320d2c8f0c19e8deb30ee10856b59ddf43a1cb
Maria del Carmen Hernandez Perez <maria-hernandez@csumb.edu>
2019-04-29 04:07:38 +0000

    Adding

--- Labs/Lab8/dbConnection.php
+++ Labs/Lab8/dbConnection.php
@@ -6,14 +6,7 @@ function getDatabaseConnection($dbname = 'lab8'){
     $username = 'root';
     $password = '';
     
-     if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
-        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-        $host = $url["host"];
-        $dbname = substr($url["path"], 1);
-        $username = $url["user"];
-        $password = $url["pass"];
-    } 
-
+     $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     
     $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
